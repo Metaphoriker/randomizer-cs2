a19419c0a7ceb9152b2ed1fae78dd4eaff857405
Luziferium <luziferisdoof@gmail.com>
2022-08-21 21:14:42 +0200

    Fixed out of bounds

--- randomizer/src/main/java/dev/luzifer/backend/event/EventExecutor.java
+++ randomizer/src/main/java/dev/luzifer/backend/event/EventExecutor.java
@@ -23,10 +23,10 @@ public class EventExecutor extends Thread {
             int random = ThreadLocalRandom.current().nextInt(0, 100);
     
             if(random >= 25) {
-                Event event = eventRepository.getEnabledEvents().get(ThreadLocalRandom.current().nextInt(0, eventRepository.getEnabledEvents().size()));
+                Event event = eventRepository.getEnabledEvents().get(ThreadLocalRandom.current().nextInt(0, eventRepository.getEnabledEvents().size() - 1));
                 EventDispatcher.dispatch(event);
             } else {
-                EventCluster eventCluster = eventClusterRepository.getClusters().get(ThreadLocalRandom.current().nextInt(0, eventClusterRepository.getClusters().size()));
+                EventCluster eventCluster = eventClusterRepository.getClusters().get(ThreadLocalRandom.current().nextInt(0, eventClusterRepository.getClusters().size() - 1));
                 for (Event event : eventCluster.getEvents())
                     EventDispatcher.dispatch(event); // TODO: don't dispatch disabled events or the entire cluster if contained
             }
