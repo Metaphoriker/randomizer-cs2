a2513ab5eaf6e450bb5356cc859afb50cb5b2faa
Luziferium <luziferisdoof@gmail.com>
2022-08-28 16:44:05 +0200

    Removed builder module for now, moved randomizer into frontend, removed modules module, made launcher and updater working properly

---
+++ randomizer-frontend/randomizer-launcher/src/main/java/dev/luzifer/launcher/Main.java
@@ -0,0 +1,66 @@
+package dev.luzifer.launcher;
+
+import dev.luzifer.model.updater.Updater;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    
+    private static final String UPDATER_VERSION_URL = "https://raw.githubusercontent.com/Luziferium/randomizer-csgo/stage/randomizer-frontend/randomizer-updater/src/main/resources/version.txt";
+    private static final String UPDATER_DOWNLOAD_URL = "https://github.com/Luziferium/randomizer-csgo/releases/download/latest/randomizer-updater.jar";
+    
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        
+        if(Collections.unmodifiableList(Arrays.asList(args)).contains("-noupdate")) {
+            
+            JFrame jFrame = new JFrame();
+            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            jFrame.setSize(200, 200);
+            jFrame.setVisible(true);
+    
+            JLabel jLabel = new JLabel();
+            jFrame.add(jLabel);
+    
+            jLabel.setText("Launcher representation...");
+        } else {
+            
+            File appdataFolder = setupAppdataFolder();
+            installUpdater(appdataFolder);
+            // TODO: check if update is needed first
+            startUpdater(appdataFolder);
+        }
+    }
+    
+    private static File setupAppdataFolder() {
+        
+        File appdataFolder = new File(System.getenv("APPDATA") + "\\randomizer");
+        appdataFolder.mkdirs();
+        
+        return appdataFolder;
+    }
+    
+    private static File installUpdater(File folderInto) {
+        
+        File updaterJar = new File(folderInto, "randomizer-updater.jar");
+        try {
+            updaterJar.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Updater.update(updaterJar, UPDATER_VERSION_URL, UPDATER_DOWNLOAD_URL);
+        return updaterJar;
+    }
+    
+    private static void startUpdater(File appdataFolder) throws IOException, URISyntaxException {
+        Runtime.getRuntime().exec("java -jar " + appdataFolder.getAbsolutePath() + "/randomizer-updater.jar" +
+                " -launcherLocation=" + Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+        System.exit(0);
+    }
+}
--- randomizer-frontend/randomizer-updater/src/main/java/dev/luzifer/updater/Main.java
+++ randomizer-frontend/randomizer-updater/src/main/java/dev/luzifer/updater/Main.java
@@ -9,12 +9,33 @@ import java.io.IOException;
 
 public class Main {
     
-    private static final String RANDOMIZER_VERSION_URL = "https://raw.githubusercontent.com/Luziferium/randomizer-csgo/stage/randomizer-model/src/main/resources/version.txt";
-    private static final String DOWNLOAD_URL = "https://github.com/Luziferium/randomizer-csgo/releases/download/latest/randomizer.jar";
+    private static final String LAUNCHER_VERSION_URL = "https://raw.githubusercontent.com/Luziferium/randomizer-csgo/stage/randomizer-frontend/randomizer-launcher/src/main/resources/version.txt";
+    private static final String LAUNCHER_DOWNLOAD_URL = "https://github.com/Luziferium/randomizer-csgo/releases/download/latest/randomizer-launcher.jar";
+    
+    private static final String RANDOMIZER_VERSION_URL = "https://raw.githubusercontent.com/Luziferium/randomizer-csgo/master/randomizer-backend/randomizer-model/src/main/resources/version.txt";
+    private static final String RANDOMIZER_DOWNLOAD_URL = "https://github.com/Luziferium/randomizer-csgo/releases/download/latest/randomizer.jar";
     private static final File RANDOMIZER_JAR = new File("randomizer.jar");
     
     public static void main(String[] args) throws IOException {
     
+        if(args.length != 0) {
+            for(int i = 0; i < args.length; i++) {
+                if(args[i].startsWith("-launcherLocation=")) {
+                    
+                    File path = new File(args[i].substring(args[i].indexOf("=") + 1));
+                    String pathName = path.getAbsolutePath().substring(0, path.getAbsolutePath().lastIndexOf("\\"));
+                    String fileName = path.getAbsolutePath().substring(path.getAbsolutePath().lastIndexOf("\\") + 1);
+                    
+                    File launcher = new File(pathName, fileName);
+                    
+                    Updater.update(launcher, LAUNCHER_VERSION_URL, LAUNCHER_DOWNLOAD_URL);
+                    Runtime.getRuntime().exec("java -jar " + launcher.getAbsolutePath() + " -noupdate");
+                    System.exit(0);
+                }
+                // TODO: parameter for randomizer stuff like -randomizerModule=module + refactor
+            }
+        }
+        
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setSize(200, 200);
@@ -25,7 +46,8 @@ public class Main {
         
         jLabel.setText("Checking for updates...");
     
-        Updater.update(RANDOMIZER_JAR, RANDOMIZER_VERSION_URL, DOWNLOAD_URL);
+        Updater.update(RANDOMIZER_JAR, RANDOMIZER_VERSION_URL, RANDOMIZER_DOWNLOAD_URL);
+        
         Runtime.getRuntime().exec("java -jar randomizer.jar");
         System.exit(0);
     }
