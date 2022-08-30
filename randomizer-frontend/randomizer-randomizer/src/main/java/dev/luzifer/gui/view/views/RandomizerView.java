d13d4e3292e615a7c7a6cd815bab8beed5e0f081
Luziferium <luziferisdoof@gmail.com>
2022-08-30 09:52:27 +0200

    Testing

--- randomizer-frontend/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
+++ randomizer-frontend/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
@@ -2,10 +2,26 @@ package dev.luzifer.gui.view.views;
 
 import dev.luzifer.gui.view.View;
 import dev.luzifer.gui.view.models.RandomizerViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class RandomizerView extends View<RandomizerViewModel> {
     
+    @FXML
+    private Pane root;
+    
     public RandomizerView(RandomizerViewModel viewModel) {
         super(viewModel);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    
+        Label label = new Label("This is a test..");
+        root.getChildren().add(label);
     }
 }
