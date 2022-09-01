a528c4abfa85baf1444107de195f1f70cd49f47f
Luziferium <luziferisdoof@gmail.com>
2022-09-01 09:55:10 +0200

    Added View to orientate at

--- randomizer-frontend/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
+++ randomizer-frontend/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
@@ -2,17 +2,11 @@ package dev.luzifer.gui.view.views;
 
 import dev.luzifer.gui.view.View;
 import dev.luzifer.gui.view.models.RandomizerViewModel;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RandomizerView extends View<RandomizerViewModel> {
-    
-    @FXML
-    private Pane root;
     
     public RandomizerView(RandomizerViewModel viewModel) {
         super(viewModel);
@@ -20,8 +14,5 @@ public class RandomizerView extends View<RandomizerViewModel> {
     
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    
-        Label label = new Label("WIP");
-        root.getChildren().add(label);
     }
 }
