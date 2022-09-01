da8a7dcaa393ff522dca617e04aa63677741b727
Luziferium <luziferisdoof@gmail.com>
2022-09-01 10:10:59 +0200

    Changed view to more or less the needs of Randomizer

--- randomizer-frontend/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
+++ randomizer-frontend/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
@@ -1,12 +1,20 @@
 package dev.luzifer.gui.view.views;
 
+import dev.luzifer.gui.util.Styling;
 import dev.luzifer.gui.view.View;
 import dev.luzifer.gui.view.models.RandomizerViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RandomizerView extends View<RandomizerViewModel> {
+    
+    @FXML
+    private TableView tableView;
     
     public RandomizerView(RandomizerViewModel viewModel) {
         super(viewModel);
