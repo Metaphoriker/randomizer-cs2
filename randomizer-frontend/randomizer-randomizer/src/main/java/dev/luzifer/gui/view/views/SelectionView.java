df11cebf94c3b43fa9ffa33baeb2958e4ef1a4a0
Luziferium <luziferisdoof@gmail.com>
2022-09-06 00:14:26 +0200

    Settings-Button is now Information-Button

--- randomizer-frontend/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/SelectionView.java
+++ randomizer-frontend/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/SelectionView.java
@@ -10,6 +10,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,7 +28,7 @@ public class SelectionView extends View<SelectionViewModel> {
     private Label builderLabel;
     
     @FXML
-    private Button settingsButton;
+    private Button informationButton;
 
     public SelectionView(SelectionViewModel selectionViewModel) {
         super(selectionViewModel);
@@ -36,8 +39,6 @@ public class SelectionView extends View<SelectionViewModel> {
 
         getIcons().add(ImageUtil.getImage("images/csgoremote_icon.png"));
 
-        // TODO: UpdateLabel
-    
         setupStyling();
         setupGraphics();
         setupMouseEvents();
@@ -49,8 +50,8 @@ public class SelectionView extends View<SelectionViewModel> {
     }
     
     @FXML
-    public void onSettingsPress(ActionEvent actionEvent) {
-        // TODO: Implement settings view
+    public void onInformationRequested(ActionEvent actionEvent) throws IOException, URISyntaxException {
+        Desktop.getDesktop().browse(new URL("https://github.com/Luziferium/randomizer-csgo/releases/tag/latest").toURI());
     }
     
     private void setupStyling() {
@@ -58,12 +59,12 @@ public class SelectionView extends View<SelectionViewModel> {
         root.setStyle(Styling.BASE);
         randomizerLabel.setStyle(Styling.UNSELECTED + Styling.BORDER);
         builderLabel.setStyle(Styling.UNSELECTED + Styling.BORDER);
-        settingsButton.setStyle(Styling.UNSELECTED + Styling.BORDER);
+        informationButton.setStyle(Styling.UNSELECTED + Styling.BORDER);
     }
     
     private void setupGraphics() {
         
-        settingsButton.setGraphic(ImageUtil.getImageView("images/settings_icon.png", ImageUtil.ImageResolution.OKAY));
+        informationButton.setGraphic(ImageUtil.getImageView("images/wip_icon.png", ImageUtil.ImageResolution.OKAY));
         randomizerLabel.setGraphic(ImageUtil.getImageView("images/shuffle_icon.png", ImageUtil.ImageResolution.SMALL));
         builderLabel.setGraphic(ImageUtil.getImageView("images/build_icon.png", ImageUtil.ImageResolution.SMALL));
     }
@@ -77,5 +78,8 @@ public class SelectionView extends View<SelectionViewModel> {
         builderLabel.setOnMouseEntered(event -> builderLabel.setStyle(Styling.SELECTED + Styling.BORDER));
         builderLabel.setOnMouseExited(event -> builderLabel.setStyle(Styling.UNSELECTED + Styling.BORDER));
         builderLabel.setOnMouseClicked(event -> getViewModel().openBuilder());
+        
+        informationButton.setOnMouseEntered(event -> informationButton.setStyle(Styling.SELECTED + Styling.BORDER));
+        informationButton.setOnMouseExited(event -> informationButton.setStyle(Styling.UNSELECTED + Styling.BORDER));
     }
 }
