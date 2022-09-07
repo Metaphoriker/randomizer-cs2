a38e0f2b4db0d989144ef87a2a101037b2335283
Luziferium <luziferisdoof@gmail.com>
2022-09-08 00:36:22 +0200

    Added and implemented BuilderView

---
+++ randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/BuilderView.java
@@ -0,0 +1,113 @@
+package dev.luzifer.gui.view.views;
+
+import dev.luzifer.gui.util.ImageUtil;
+import dev.luzifer.gui.util.Styling;
+import dev.luzifer.gui.view.View;
+import dev.luzifer.gui.view.models.BuilderViewModel;
+import dev.luzifer.model.event.Event;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BuilderView extends View<BuilderViewModel> {
+    
+    @FXML
+    private HBox buttonHBox;
+    
+    @FXML
+    private VBox buttonVBox;
+    
+    @FXML
+    private VBox clusterBuilderVBox;
+    
+    @FXML
+    private VBox clusterVBox;
+    
+    @FXML
+    private FlowPane eventFlowPane;
+    
+    @FXML
+    private VBox root;
+    
+    @FXML
+    private Button randomizerButton;
+    
+    @FXML
+    private Button saveButton;
+    
+    @FXML
+    private Button clearButton;
+    
+    public BuilderView(BuilderViewModel viewModel) {
+        super(viewModel);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+        getIcons().add(ImageUtil.getImage("images/build_icon.png"));
+        
+        root.setStyle(Styling.BASE_DARKER);
+        
+        buttonHBox.setStyle(Styling.BORDER);
+        buttonVBox.setStyle(Styling.BORDER);
+        
+        randomizerButton.setGraphic(ImageUtil.getImageView("images/shuffle_icon.png", ImageUtil.ImageResolution.SMALL));
+        saveButton.setGraphic(ImageUtil.getImageView("images/plus_icon.png", ImageUtil.ImageResolution.SMALL));
+        clearButton.setGraphic(ImageUtil.getImageView("images/delete_icon.png", ImageUtil.ImageResolution.SMALL));
+        
+        for (Event event : getViewModel().getEvents()) {
+            
+            Label label = new Label(event.name());
+            label.setStyle(Styling.BORDER + Styling.HEADER);
+            label.setContentDisplay(ContentDisplay.RIGHT);
+            label.setPadding(new Insets(0, 0, 0, 5));
+            label.setGraphic(ImageUtil.getImageView("images/drag_icon.png", ImageUtil.ImageResolution.SMALL));
+            label.getGraphic().setOpacity(0);
+            
+            label.setOnMouseEntered(mouseEvent -> label.getGraphic().setOpacity(1));
+            label.setOnMouseExited(mouseEvent -> label.getGraphic().setOpacity(0));
+            
+            eventFlowPane.getChildren().add(label);
+        }
+    }
+    
+    @FXML
+    public void onRandomize(ActionEvent actionEvent) {
+        
+        clusterBuilderVBox.getChildren().clear();
+        
+        for (int i = 0; i < ThreadLocalRandom.current().nextInt(1, 10); i++) {
+            
+            int randomEventIndex = ThreadLocalRandom.current().nextInt(0, getViewModel().getEvents().size());
+            Event randomEvent = getViewModel().getEvents().get(randomEventIndex);
+            
+            clusterBuilderVBox.getChildren().add(new Label(randomEvent.name()));
+        }
+    }
+    
+    @FXML
+    public void onSave(ActionEvent actionEvent) {
+        
+        if (clusterBuilderVBox.getChildren().isEmpty())
+            return;
+        
+        clusterBuilderVBox.getChildren().clear();
+        clusterVBox.getChildren().add(new Label("Insert stuff here"));
+    }
+    
+    @FXML
+    public void onClear(ActionEvent actionEvent) {
+        clusterBuilderVBox.getChildren().clear();
+    }
+}
