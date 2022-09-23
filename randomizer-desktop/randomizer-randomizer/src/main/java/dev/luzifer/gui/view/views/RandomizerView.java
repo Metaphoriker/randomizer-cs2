66dfc6642edcdf0529bbb0c141356203351e6550
Luziferium <luziferisdoof@gmail.com>
2022-09-23 23:38:18 +0200

    Wiped randomizer view

--- randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
+++ randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
@@ -1,29 +1,11 @@
 package dev.luzifer.gui.view.views;
 
-import dev.luzifer.gui.util.ImageUtil;
 import dev.luzifer.gui.view.View;
 import dev.luzifer.gui.view.models.RandomizerViewModel;
-import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class RandomizerView extends View<RandomizerViewModel> {
     
-    @FXML
-    private Tab configTab;
-    
-    @FXML
-    private Tab randomizerTab;
-    
     public RandomizerView(RandomizerViewModel viewModel) {
         super(viewModel);
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        randomizerTab.setGraphic(ImageUtil.getImageView("images/shuffle_icon.png", ImageUtil.ImageResolution.SMALL));
-        configTab.setGraphic(ImageUtil.getImageView("images/settings_icon.png", ImageUtil.ImageResolution.SMALL));
     }
 }
