8b145a68a3552e241a6261e2efeea72c69e81632
Luziferium <luziferisdoof@gmail.com>
2022-09-30 12:14:26 +0200

    Template design

--- randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
+++ randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
@@ -2,10 +2,28 @@ package dev.luzifer.gui.view.views;
 
 import dev.luzifer.gui.view.View;
 import dev.luzifer.gui.view.models.RandomizerViewModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 public class RandomizerView extends View<RandomizerViewModel> {
-    
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    private VBox logVBox;
+
+    @FXML
+    private Button toggleButton;
+
     public RandomizerView(RandomizerViewModel viewModel) {
         super(viewModel);
+    }
+
+    @FXML
+    private void onToggle(ActionEvent actionEvent) {
+
     }
 }
