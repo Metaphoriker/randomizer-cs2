c49dd460d5f03b108d5666a51321bd2aedf31ae7
Luziferium <luziferisdoof@gmail.com>
2022-10-02 19:45:52 +0200

    Now randomizer shows something and cant deactivate itself anymore

--- randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
+++ randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
@@ -37,43 +37,25 @@ public class RandomizerView extends View<RandomizerViewModel> {
     
         toggleButton.textProperty().bindBidirectional(getViewModel().getNextStateProperty());
         
-        /* TODO: dev nodes on german, y'all suck
-         *  - fuehrt aktuell 6x das MouseLeftClickEvent aus, da der den Handler dafür 3x registriert
-         *  - event API überarbeiten, die Events sind alle die gleiche instanz, was sich mit dem EventDispatcher nicht verträgt
-         *  - nvm. vergiss das was oben steht, ich bin einfach dumm und führ das immer wieder aus und registriere immer wieder handler aufs neue
-         *  - nvm nvm, keine ahnung was kaputt ist
-         */
         for(EventCluster cluster : getViewModel().getClusters()) {
             
             EventDispatcher.registerGenericClusterHandler(cluster, ec -> {
                 Platform.runLater(() -> {
-                    logVBox.getChildren().add(new Label(cluster.getName()));
+                    
+                    Label label = new Label(cluster.getName());
+                    label.setGraphic(ImageUtil.getImageView("images/loading_gif.gif"));
+                    logVBox.getChildren().add(label);
                 });
             });
-    
-            for(Event event : cluster.getEvents()) {
-                EventDispatcher.registerHandler(event.getClass(), e -> {
-                    Platform.runLater(() -> {
-                
-                        Label label = new Label(event.getClass().getSimpleName());
-                        label.setGraphic(ImageUtil.getImageView("images/loading_gif.gif"));
-                
-                        logVBox.getChildren().add(label);
-                    });
+            
+            EventDispatcher.registerOnFinish(cluster, ec -> {
+                Platform.runLater(() -> {
+                    
+                    Label label = (Label) logVBox.getChildren().get(logVBox.getChildren().size() - 1);
+                    label.setGraphic(ImageUtil.getImageView("images/checkmark_icon.png"));
+                    logVBox.getChildren().set(logVBox.getChildren().size() - 1, label);
                 });
-        
-                EventDispatcher.registerOnFinish(event,  e -> {
-                    Platform.runLater(() -> {
-                        logVBox.getChildren().stream()
-                                .filter(node -> node instanceof Label)
-                                .filter(node -> ((Label) node).getText().equals(event.getClass().getSimpleName()))
-                                .map(node -> (Label) node)
-                                .findFirst().ifPresent(node -> {
-                                    node.setGraphic(ImageUtil.getImageView("images/checkmark_icon.png"));
-                                });
-                    });
-                });
-            }
+            });
         }
     }
     
