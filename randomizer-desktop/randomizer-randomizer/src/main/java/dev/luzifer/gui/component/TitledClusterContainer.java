cee65c3addf89d8e8f853cae650fe17e9b4b3a19
Luziferium <luziferisdoof@gmail.com>
2022-10-02 23:03:46 +0200

    Now cool animation haha, custom component haha, titled pane haha, cool fidget spinner goes brrrrrrrrrrrrrrrrrrr

---
+++ randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/component/TitledClusterContainer.java
@@ -0,0 +1,58 @@
+package dev.luzifer.gui.component;
+
+import dev.luzifer.gui.util.ImageUtil;
+import dev.luzifer.model.event.Event;
+import dev.luzifer.model.event.cluster.EventCluster;
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.VBox;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TitledClusterContainer extends TitledPane {
+    
+    private final VBox vBox = new VBox();
+    private final EventCluster eventCluster;
+    
+    public TitledClusterContainer(String title, EventCluster eventCluster) {
+        
+        this.eventCluster = eventCluster;
+        
+        setText(title);
+        setContent(vBox);
+        setGraphic(ImageUtil.getImageView("images/loading_gif.gif", ImageUtil.ImageResolution.MEDIUM));
+        
+        fill(eventCluster);
+    }
+    
+    public List<Label> getEventLabels() {
+        return vBox.getChildren().stream().filter(Label.class::isInstance).map(Label.class::cast).collect(Collectors.toList());
+    }
+    
+    public void finish() {
+        setGraphic(ImageUtil.getImageView("images/checkmark_icon.png", ImageUtil.ImageResolution.MEDIUM));
+    }
+    
+    public void finish(Event event) {
+        getEventLabels().forEach(node -> {
+            if(node.getText().equals(event.name()))
+                node.setGraphic(ImageUtil.getImageView("images/checkmark_icon.png", ImageUtil.ImageResolution.SMALL));
+        });
+    }
+    
+    public EventCluster getEventCluster() {
+        return eventCluster;
+    }
+    
+    private void fill(EventCluster eventCluster) {
+        Arrays.stream(eventCluster.getEvents()).forEach(event -> {
+            
+            Label label = new Label(event.name());
+            label.setGraphic(ImageUtil.getImageView("images/loading_gif.gif", ImageUtil.ImageResolution.SMALL));
+            
+            vBox.getChildren().add(label);
+        });
+    }
+}
--- randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
+++ randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/view/views/RandomizerView.java
@@ -1,6 +1,6 @@
 package dev.luzifer.gui.view.views;
 
-import dev.luzifer.gui.util.ImageUtil;
+import dev.luzifer.gui.component.TitledClusterContainer;
 import dev.luzifer.gui.view.View;
 import dev.luzifer.gui.view.models.RandomizerViewModel;
 import dev.luzifer.model.event.EventDispatcher;
@@ -8,7 +8,6 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -36,54 +35,39 @@ public class RandomizerView extends View<RandomizerViewModel> {
         toggleButton.textProperty().bindBidirectional(getViewModel().getNextStateProperty());
     
         // TODO: viewmodel stuff
-        EventDispatcher.registerGenericHandler(event -> {
-            Platform.runLater(() -> {
-                
-                Label label = new Label(event.name());
-                label.setFont(label.getFont().font(16));
-                label.setGraphic(ImageUtil.getImageView("images/loading_gif.gif", ImageUtil.ImageResolution.SMALL));
-                
-                logVBox.getChildren().add(label);
-            });
-        });
-        
         getViewModel().getEvents().forEach(event -> {
             EventDispatcher.registerOnFinish(event, e -> {
                 Platform.runLater(() -> {
-                    
-                    Label label = (Label) logVBox.getChildren().stream()
-                            .filter(node -> node instanceof Label)
-                            .filter(node -> ((Label) node).getText().equals(event.name()))
+    
+                    TitledClusterContainer titledClusterContainer = (TitledClusterContainer) logVBox.getChildren().stream()
+                            .filter(TitledClusterContainer.class::isInstance)
+                            .filter(node -> ((TitledClusterContainer) node).getEventLabels().stream()
+                                    .anyMatch(label -> label.getText().equals(event.name())))
                             .reduce((first, second) -> second)
                             .orElse(null);
-                    
-                    if(label != null)
-                        label.setGraphic(ImageUtil.getImageView("images/checkmark_icon.png", ImageUtil.ImageResolution.SMALL));
+    
+                    if(titledClusterContainer != null)
+                        titledClusterContainer.finish(event);
                 });
             });
         });
     
         getViewModel().getClusters().forEach(cluster -> {
             EventDispatcher.registerGenericClusterHandler(cluster, ec -> Platform.runLater(() -> {
-        
-                logVBox.getChildren().clear(); // TODO: not really "logging" anymore
-                
-                Label label = new Label(cluster.getName());
-                label.setFont(label.getFont().font(32));
-                label.setGraphic(ImageUtil.getImageView("images/loading_gif.gif", ImageUtil.ImageResolution.MEDIUM));
-                
-                logVBox.getChildren().add(label);
+                TitledClusterContainer titledClusterContainer = new TitledClusterContainer(cluster.getName(), cluster);
+                logVBox.getChildren().add(titledClusterContainer);
             }));
     
             EventDispatcher.registerOnFinish(cluster, ec -> Platform.runLater(() -> {
-        
-                logVBox.getChildren().forEach(node -> {
-                    if(node instanceof Label) {
-                        Label label = (Label) node;
-                        if(label.getText().equals(cluster.getName()))
-                            label.setGraphic(ImageUtil.getImageView("images/checkmark_icon.png", ImageUtil.ImageResolution.MEDIUM));
-                    }
-                });
+                
+                TitledClusterContainer titledClusterContainer = (TitledClusterContainer) logVBox.getChildren().stream()
+                        .filter(TitledClusterContainer.class::isInstance)
+                        .filter(node -> ((TitledClusterContainer) node).getEventCluster().equals(cluster))
+                        .reduce((first, second) -> second)
+                        .orElse(null);
+                
+                if(titledClusterContainer != null)
+                    titledClusterContainer.finish();
             }));
         });
     }
