5b45c3ef467d475f0f3dcf43c1436a32083490bb
Luziferium <luziferisdoof@gmail.com>
2022-10-04 11:01:29 +0200

    Fixed logging

--- randomizer-model/src/main/java/dev/luzifer/model/exception/UncaughtExceptionLogger.java
+++ randomizer-model/src/main/java/dev/luzifer/model/exception/UncaughtExceptionLogger.java
@@ -4,18 +4,20 @@ import dev.luzifer.model.stuff.WhateverThisFuckerIs;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DateFormat;
 
 public class UncaughtExceptionLogger implements Thread.UncaughtExceptionHandler {
 
     public static final UncaughtExceptionLogger DEFAULT_UNCAUGHT_EXCEPTION_LOGGER = new UncaughtExceptionLogger();
-    
-    private static final DateFormat DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM);
-    
+
     private static final File LOG_FOLDER = new File(WhateverThisFuckerIs.getAppdataFolder() + File.separator + "logs");
-    private static final File LOG_FILE = new File(LOG_FOLDER, DATE_FORMAT.format(System.currentTimeMillis()) + ".log");
-    
+
+    static {
+        LOG_FOLDER.mkdirs();
+    }
+
     @Override
     public void uncaughtException(Thread thread, Throwable throwable) {
         
@@ -28,16 +30,14 @@ public class UncaughtExceptionLogger implements Thread.UncaughtExceptionHandler 
     
     private void log(String message) {
         
-        if(!LOG_FILE.exists()) {
-            LOG_FOLDER.mkdirs();
-            try {
-                LOG_FILE.createNewFile();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+        File logFile = new File(LOG_FOLDER, DateFormat.getDateInstance().format(System.currentTimeMillis()) + ".log");
+        try {
+            logFile.createNewFile();
+        } catch (IOException e) {
+            // well, fuck
         }
-        
-        try(PrintWriter printWriter = new PrintWriter(new FileOutputStream(LOG_FILE, true))) {
+
+        try(PrintWriter printWriter = new PrintWriter(new FileOutputStream(logFile, true))) {
             printWriter.println(message);
             printWriter.flush();
         } catch (Exception e) {
