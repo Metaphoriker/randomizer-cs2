da58ab551498fb4ee102da0d721ab25d28afe915
Luziferium <luziferisdoof@gmail.com>
2022-10-19 18:15:50 +0200

    Gave it a bit of styling

--- randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/component/EventSettingsComponent.java
+++ randomizer-desktop/randomizer-randomizer/src/main/java/dev/luzifer/gui/component/EventSettingsComponent.java
@@ -13,6 +13,13 @@ public class EventSettingsComponent extends VBox {
     public EventSettingsComponent(Event event) {
         
         title.setText(event.name() + " Settings");
+        title.setStyle("" +
+                "-fx-font-weight: bold;" +
+                " -fx-font-size: 12px;" +
+                " -fx-padding: 10px 0 0 0;" +
+                " -fx-border-color: black;" +
+                " -fx-border-width: 0 0 1 0;" +
+                " -fx-background-color: cyan;");
         
         // Build settings
         
