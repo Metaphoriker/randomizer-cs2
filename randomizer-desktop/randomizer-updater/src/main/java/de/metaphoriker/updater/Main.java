190c24552ce994f8eb98635fbe7857a4814aa266
Metaphoriker <luziferisdoof@gmail.com>
2024-10-09 01:58:31 +0200

    feat: new updater design

--- randomizer-desktop/randomizer-updater/src/main/java/de/metaphoriker/updater/Main.java
+++ randomizer-desktop/randomizer-updater/src/main/java/de/metaphoriker/updater/Main.java
@@ -2,33 +2,48 @@ package de.metaphoriker.updater;
 
 import de.metaphoriker.model.exception.UncaughtExceptionLogger;
 import de.metaphoriker.model.updater.Updater;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 import javax.swing.*;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
 
     Thread.currentThread()
         .setUncaughtExceptionHandler(UncaughtExceptionLogger.DEFAULT_UNCAUGHT_EXCEPTION_LOGGER);
 
-    JFrame jFrame = new JFrame("Updater");
-    jFrame.setSize(500, 100);
+    JFrame jFrame = new JFrame();
+    jFrame.setSize(500, 200);
+    jFrame.setUndecorated(true);
     jFrame.setLocationRelativeTo(null);
     jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jFrame.getContentPane().setBackground(new Color(34, 45, 50));
+
+    JPanel panel = new JPanel(new BorderLayout());
+    panel.setBackground(new Color(34, 45, 50));
+
+    JProgressBar progressBar = new JProgressBar(0, 100);
+    progressBar.setStringPainted(true);
+    progressBar.setForeground(new Color(77, 182, 172));
+    progressBar.setBackground(new Color(44, 62, 80));
+    progressBar.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+    JLabel jLabel = new JLabel("Checking for updates...", SwingConstants.CENTER);
+    jLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+    jLabel.setForeground(Color.WHITE);
+
+    panel.add(jLabel, BorderLayout.NORTH);
+    panel.add(progressBar, BorderLayout.CENTER);
+
+    jFrame.add(panel);
     jFrame.setVisible(true);
-
-    JLabel jLabel = new JLabel();
-    jLabel.setIcon(new ImageIcon(Main.class.getResource("images/loading_gif.gif")));
-    jFrame.add(jLabel);
-
-    jLabel.setText("Checking for updates...");
 
     if (args.length != 0) {
       for (String arg : args) {
         if (arg.startsWith("-randomizerLocation=")) {
-
           File path = new File(arg.substring(arg.indexOf("=") + 1));
           String pathName =
               path.getAbsolutePath().substring(0, path.getAbsolutePath().lastIndexOf("\\"));
@@ -37,27 +52,62 @@ public class Main {
 
           File randomizer = new File(pathName, fileName);
 
-          if (Updater.isUpdateAvailable(randomizer, Updater.RANDOMIZER_VERSION_URL)) {
-            jLabel.setText("Updating...");
-            Updater.update(
-                randomizer,
-                Updater.RANDOMIZER_DOWNLOAD_URL,
-                (bytesRead, totalBytes) -> {
-                  double progress = (double) bytesRead / totalBytes * 100;
-                  jLabel.setText("Downloading... " + String.format("%.2f", progress) + "%");
-                });
-            jLabel.setText("Update successful!");
-          } else {
-            jLabel.setText("No update available!");
-          }
-          jLabel.setIcon(new ImageIcon(Main.class.getResource("images/checkmark_icon.png")));
-          Runtime.getRuntime().exec("java -jar " + randomizer.getAbsolutePath());
+          CompletableFuture.supplyAsync(
+                  () -> Updater.isUpdateAvailable(randomizer, Updater.RANDOMIZER_VERSION_URL))
+              .thenAccept(
+                  isUpdateAvailable -> {
+                    if (isUpdateAvailable) {
+                      jLabel.setText("Update available. Updating...");
+                      CompletableFuture.runAsync(
+                              () -> {
+                                Updater.update(
+                                    randomizer,
+                                    Updater.RANDOMIZER_DOWNLOAD_URL,
+                                    (bytesRead, totalBytes) -> {
+                                      double progress = (double) bytesRead / totalBytes * 100;
+                                      SwingUtilities.invokeLater(
+                                          () -> {
+                                            progressBar.setValue((int) progress);
+                                            progressBar.setString(
+                                                "Downloading... "
+                                                    + String.format("%.2f", progress)
+                                                    + "%");
+                                          });
+                                    });
+                              })
+                          .thenRun(
+                              () -> {
+                                SwingUtilities.invokeLater(
+                                    () -> {
+                                      jLabel.setText("Update successful!");
+                                      progressBar.setValue(100);
+                                      progressBar.setString("Completed!");
+                                      jLabel.setIcon(
+                                          new ImageIcon(
+                                              Main.class.getResource("images/checkmark_icon.png")));
+                                    });
+                                try {
+                                  Runtime.getRuntime()
+                                      .exec("java -jar " + randomizer.getAbsolutePath());
+                                  Thread.sleep(2000);
+                                  System.exit(0);
+                                } catch (IOException | InterruptedException e) {
+                                  e.printStackTrace();
+                                }
+                              });
+                    } else {
+                      SwingUtilities.invokeLater(() -> jLabel.setText("No update available!"));
+                      progressBar.setValue(100);
+                      progressBar.setString("Completed!");
+                      jLabel.setIcon(
+                          new ImageIcon(Main.class.getResource("images/checkmark_icon.png")));
+                    }
+                  });
           break;
         }
       }
     } else {
       jLabel.setText("No randomizer location specified! DONT OPEN THIS JAR DIRECTLY!");
     }
-    System.exit(0);
   }
 }
