ae6d325beb3cecc85965b4fa69e7a4b0674af6a0
Metaphoriker <luziferisdoof@gmail.com>
2024-10-08 04:09:22 +0200

    fix: now updates the wait time immediately

--- randomizer-model/src/main/java/de/metaphoriker/model/event/EventExecutorRunnable.java
+++ randomizer-model/src/main/java/de/metaphoriker/model/event/EventExecutorRunnable.java
@@ -11,36 +11,54 @@ public class EventExecutorRunnable implements Runnable {
   private static volatile int maxWaitTime = 120 * 1000;
   private final EventClusterRepository eventClusterRepository;
 
+  private static volatile boolean waitTimeUpdated = false;
+
   public EventExecutorRunnable(EventClusterRepository eventClusterRepository) {
     this.eventClusterRepository = eventClusterRepository;
   }
 
-  public static void setMaxWaitTime(int maxWaitTime) {
-    EventExecutorRunnable.maxWaitTime = maxWaitTime;
+  public static void setMaxWaitTime(int newMaxWaitTime) {
+    maxWaitTime = newMaxWaitTime;
+    waitTimeUpdated = true;
+    notifyThread();
   }
 
-  public static void setMinWaitTime(int minWaitTime) {
-    EventExecutorRunnable.minWaitTime = minWaitTime;
+  public static void setMinWaitTime(int newMinWaitTime) {
+    minWaitTime = newMinWaitTime;
+    waitTimeUpdated = true;
+    notifyThread();
+  }
+
+  private static void notifyThread() {
+    synchronized (EventExecutorRunnable.class) {
+      EventExecutorRunnable.class.notifyAll();
+    }
   }
 
   @Override
   public void run() {
-
     while (true) {
-
       if (WhateverThisFuckerIs.getApplicationState() == ApplicationState.RUNNING
-          && !eventClusterRepository.getClusters().isEmpty())
+          && !eventClusterRepository.getClusters().isEmpty()) {
+
         EventDispatcher.dispatchCluster(
             eventClusterRepository
                 .getClusters()
                 .get(
                     ThreadLocalRandom.current()
                         .nextInt(0, eventClusterRepository.getClusters().size())));
+      }
 
-      try {
-        Thread.sleep(ThreadLocalRandom.current().nextInt(minWaitTime, maxWaitTime));
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
+      synchronized (EventExecutorRunnable.class) {
+        if (waitTimeUpdated) {
+          waitTimeUpdated = false;
+        }
+        int waitTime = ThreadLocalRandom.current().nextInt(minWaitTime, maxWaitTime);
+        try {
+          EventExecutorRunnable.class.wait(waitTime);
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
       }
     }
   }
