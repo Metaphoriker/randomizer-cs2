cef60deb5b61368fc3e417133a27b3ef3035722c
Metaphoriker <luziferisdoof@gmail.com>
2024-10-13 15:11:00 +0200

    refact: ActionRegistryStorage now works with the name of the actions

--- randomizer-model/src/main/java/de/metaphoriker/model/action/handling/ActionRegistryStorage.java
+++ randomizer-model/src/main/java/de/metaphoriker/model/action/handling/ActionRegistryStorage.java
@@ -21,33 +21,36 @@ public class ActionRegistryStorage {
       new File(ApplicationContext.getAppdataFolder() + File.separator + "actions.json");
   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-  public Map<Action, Boolean> load() {
+  public Map<String, Boolean> load() {
     if (!STORAGE_FILE.exists()) {
       return new HashMap<>();
     }
 
     try {
-      return OBJECT_MAPPER.readValue(STORAGE_FILE, new TypeReference<Map<Action, Boolean>>() {});
+      return OBJECT_MAPPER.readValue(STORAGE_FILE, new TypeReference<>() {});
     } catch (IOException e) {
       throw new RuntimeException("Failed to load actions from file", e);
     }
   }
 
   public void save(Map<Action, Boolean> actions) {
+    Map<String, Boolean> actionStates = new HashMap<>();
+    actions.forEach((action, enabled) -> actionStates.put(action.name(), enabled));
+
     try {
-      OBJECT_MAPPER.writeValue(STORAGE_FILE, actions);
+      OBJECT_MAPPER.writeValue(STORAGE_FILE, actionStates);
     } catch (IOException e) {
       throw new RuntimeException("Failed to save actions to file", e);
     }
   }
 
   public void loadStates(Map<Action, Boolean> actions) {
-    Map<Action, Boolean> loadedActions = load();
+    Map<String, Boolean> loadedActions = load();
     actions
         .keySet()
         .forEach(
             action -> {
-              Boolean enabled = loadedActions.get(action);
+              Boolean enabled = loadedActions.get(action.name());
               if (enabled != null) {
                 actions.put(action, enabled);
               }
