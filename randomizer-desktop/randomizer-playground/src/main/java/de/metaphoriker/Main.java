e87e3abfdfe016463459c30c00efed68018f986d
Metaphoriker <luziferisdoof@gmail.com>
2024-10-14 01:23:20 +0200

    refact: move JavaFX startup logic to RandomizerStarter class
    Moved the JavaFX initialization and startup logic from the Main class to a new RandomizerStarter class for better separation of concerns. This enhances code maintainability and adheres to the single responsibility principle.

--- randomizer-desktop/randomizer-playground/src/main/java/de/metaphoriker/Main.java
+++ randomizer-desktop/randomizer-playground/src/main/java/de/metaphoriker/Main.java
@@ -1,43 +1,10 @@
 package de.metaphoriker;
 
-import de.metaphoriker.util.ImageUtil;
-import de.metaphoriker.view.ViewProvider;
-import de.metaphoriker.view.views.MainWindow;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-
-  private final ViewProvider viewProvider = ViewProvider.getInstance();
-
-  @Override
-  public void start(Stage stage) {
-    try {
-      Parent root = viewProvider.requestView(MainWindow.class);
-
-      Scene scene = new Scene(root);
-      scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-      stage.setTitle("Randomizer Playground");
-      stage.setScene(scene);
-      stage.getIcons().add(ImageUtil.getImage("images/randomizerLogo.jpg"));
-      stage.show();
-
-      Platform.runLater(() -> setMinResizable(stage));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void setMinResizable(Stage stage) {
-    stage.setMinWidth(800);
-    stage.setMinHeight(600);
-    stage.setResizable(true);
-  }
+public class Main {
 
   public static void main(String[] args) {
-    launch(args);
+    Application.launch(RandomizerStarter.class);
   }
 }
