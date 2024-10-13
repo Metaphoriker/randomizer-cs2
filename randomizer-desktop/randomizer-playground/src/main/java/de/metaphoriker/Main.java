0faae9735bd4b41be9c91534ccf7d36f3886a26e
Metaphoriker <luziferisdoof@gmail.com>
2024-10-13 20:12:07 +0200

    playground: min resize is now at 800x600

--- randomizer-desktop/randomizer-playground/src/main/java/de/metaphoriker/Main.java
+++ randomizer-desktop/randomizer-playground/src/main/java/de/metaphoriker/Main.java
@@ -3,6 +3,7 @@ package de.metaphoriker;
 import de.metaphoriker.view.views.MainWindow;
 import de.metaphoriker.view.ViewProvider;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -20,9 +21,17 @@ public class Main extends Application {
       scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
       stage.setScene(scene);
       stage.show();
+
+      Platform.runLater(() -> setMinResizable(stage));
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private void setMinResizable(Stage stage) {
+    stage.setMinWidth(800);
+    stage.setMinHeight(600);
+    stage.setResizable(true);
   }
 
   public static void main(String[] args) {
