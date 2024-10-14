0e15ee2bfedbe45244c4a7feeaa2ca9c2e57999f
Metaphoriker <luziferisdoof@gmail.com>
2024-10-14 03:34:07 +0200

    refact: Extract and enhance uncaught exception logging
    Refactored `uncaughtException` method by extracting the exception message building logic into a separate method. Enhanced logging to include debug messages when log files are created and when messages are written to the file. Simplified file creation using `Files.createFile`.

--- randomizer-model/src/main/java/de/metaphoriker/model/exception/UncaughtExceptionLogger.java
+++ randomizer-model/src/main/java/de/metaphoriker/model/exception/UncaughtExceptionLogger.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +18,6 @@ public class UncaughtExceptionLogger implements Thread.UncaughtExceptionHandler 
 
   private static final File LOG_FOLDER =
       new File(ApplicationContext.getAppdataFolder() + File.separator + "logs");
-
   private static final SimpleDateFormat LOG_DATE_FORMAT =
       new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 
@@ -27,8 +27,12 @@ public class UncaughtExceptionLogger implements Thread.UncaughtExceptionHandler 
 
   @Override
   public void uncaughtException(Thread thread, Throwable throwable) {
+    String exceptionMessage = buildExceptionMessage(thread, throwable);
+    logToFile(exceptionMessage);
+  }
 
-    StringBuilder builder =
+  private String buildExceptionMessage(Thread thread, Throwable throwable) {
+    StringBuilder exceptionMessageBuilder =
         new StringBuilder()
             .append("Uncaught exception in thread \"")
             .append(thread.getName())
@@ -39,31 +43,29 @@ public class UncaughtExceptionLogger implements Thread.UncaughtExceptionHandler 
             .append("\n");
 
     for (StackTraceElement element : throwable.getStackTrace()) {
-      builder.append("\tat ").append(element.toString()).append("\n");
+      exceptionMessageBuilder.append("\tat ").append(element.toString()).append("\n");
     }
 
     if (throwable.getCause() != null) {
-      builder.append("Caused by: ").append(throwable.getCause()).append("\n");
+      exceptionMessageBuilder.append("Caused by: ").append(throwable.getCause()).append("\n");
     }
-
-    logToFile(builder.toString());
+    return exceptionMessageBuilder.toString();
   }
 
   private void logToFile(String message) {
     String logFileName = LOG_DATE_FORMAT.format(new Date()) + ".log";
     File logFile = new File(LOG_FOLDER, logFileName);
-
     try {
       if (!logFile.exists()) {
-        logFile.createNewFile();
+        Files.createFile(logFile.toPath());
+        log.debug("Log file wurde angelegt: {}", logFile.getAbsolutePath());
       }
-
       try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(logFile, true))) {
         printWriter.println(message);
       }
-
+      log.debug("Nachricht {} wurde zur Datei {} geschrieben", message, logFile.getAbsolutePath());
     } catch (IOException e) {
-      log.error("Failed to log exception to file: " + logFileName, e);
+      log.error("Failed to log exception to file: {}", logFileName, e);
     }
   }
 }
