cf0c1a4748326905afc7750cb8c4a53a9bc785bb
Metaphoriker <luziferisdoof@gmail.com>
2024-11-11 23:36:43 +0100

    fix: Add fx:id and controller to HomeView.fxml
    Added fx:id attribute to the root AnchorPane in HomeView.fxml and assigned the controller to HomeViewController. Also introduced initialization logic in HomeViewController using Platform.runLater.

--- randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/HomeViewController.java
+++ randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/HomeViewController.java
@@ -1,7 +1,20 @@
 package com.revortix.randomizer.ui.view.controller;
 
 import com.revortix.randomizer.ui.view.View;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 @View
 public class HomeViewController {
+
+    @FXML
+    private AnchorPane root;
+
+    public HomeViewController() {
+        Platform.runLater(this::initialize);
+    }
+
+    private void initialize() {
+    }
 }
