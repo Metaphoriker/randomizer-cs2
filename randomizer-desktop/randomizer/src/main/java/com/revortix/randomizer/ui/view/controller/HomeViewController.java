e695a661393585e05750755565d40cca703d2f8c
Metaphoriker <luziferisdoof@gmail.com>
2024-11-14 13:50:09 +0100

    feat: Add event handlers for opening Discord and GitHub links
    Added onDiscordOpen and onGitHubOpen methods with mouse click event handlers to HomeViewController. Implemented error handling for internet connectivity issues with showAlertInternetConnection method. Updated HomeView.fxml to include onMouseClicked attributes for Discord and GitHub labels.

--- randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/HomeViewController.java
+++ randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/HomeViewController.java
@@ -1,26 +1,56 @@
 package com.revortix.randomizer.ui.view.controller;
 
 import com.google.inject.Inject;
+import com.revortix.randomizer.ui.RandomizerApplication;
 import com.revortix.randomizer.ui.view.View;
 import com.revortix.randomizer.ui.view.viewmodel.HomeViewModel;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+
+@Slf4j
 @View
 public class HomeViewController {
 
     private final HomeViewModel homeViewModel;
 
-    @FXML
-    private AnchorPane root;
-
     @Inject
     public HomeViewController(HomeViewModel homeViewModel) {
         this.homeViewModel = homeViewModel;
-        Platform.runLater(this::initialize);
     }
 
-    private void initialize() {
+    @FXML
+    void onDiscordOpen(MouseEvent event) {
+        try {
+            homeViewModel.openDiscord();
+        } catch (IOException e) {
+            log.error("Error opening discord", e);
+            showAlertInternetConnection();
+        }
+    }
+
+    @FXML
+    void onGitHubOpen(MouseEvent event) {
+        try {
+            homeViewModel.openGitHub();
+        } catch (IOException e) {
+            log.error("Error opening github", e);
+            showAlertInternetConnection();
+        }
+    }
+
+    private void showAlertInternetConnection() {
+        Platform.runLater(() -> {
+            Alert alert = new Alert(javafx.scene.control.Alert.AlertType.ERROR);
+            alert.getDialogPane().getStylesheets().add(RandomizerApplication.class.getResource("alert-style.css").toExternalForm());
+            alert.setTitle("Internet Connectivity Issue");
+            alert.setHeaderText(null);
+            alert.setContentText("Please check your internet connection and try again.");
+            alert.showAndWait();
+        });
     }
 }
