dbb3cc6939ba0b5f9b57ee36b2ace6547184bc83
Metaphoriker <luziferisdoof@gmail.com>
2025-03-03 23:02:12 +0100

    fix: positional styling concurrent modification exception - racing condition

--- randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/RandomizerViewController.java
+++ randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/RandomizerViewController.java
@@ -13,7 +13,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -152,38 +151,21 @@ public class RandomizerViewController {
 
   private void setPositionalStyling(Label label, boolean active) {
     int index = actionsVBox.getChildren().indexOf(label);
-    ObservableList<Node> children = actionsVBox.getChildren(); // Hole die Liste nur einmal
-
-    if (children.isEmpty()) { // Überprüfe, ob die Liste leer ist
-      // Optional: Logging, um zu verstehen, wann dies passiert
-      // System.err.println("actionsVBox ist leer in setPositionalStyling!");
-      return; // Beende die Methode, wenn die Liste leer ist.
-    }
+    ObservableList<Node> children = actionsVBox.getChildren();
+    ObservableList<String> styleClasses = label.getStyleClass();
 
     if (index == 0) {
-      label.getStyleClass().clear();
-      label.getStyleClass().add(ACTION_NAME_STYLING);
-      label
-          .getStyleClass()
-          .add(active ? START_ACTIVE_ACTION_NAME_STYLING : START_ACTION_NAME_STYLING);
-      return;
-    }
-
-    if (index == children.size() - 1) {
-      label.getStyleClass().clear();
-      label.getStyleClass().add(ACTION_NAME_STYLING);
-      label.getStyleClass().add(active ? END_ACTIVE_ACTION_NAME_STYLING : END_ACTION_NAME_STYLING);
-      return;
-    }
-
-    if (index > 0 && index < children.size() - 1) { // Stelle sicher index ist im Rahmen.
-      label.getStyleClass().clear();
-      label.getStyleClass().add(ACTION_NAME_STYLING);
-      label
-          .getStyleClass()
-          .add(active ? MIDDLE_ACTIVE_ACTION_NAME_STYLING : MIDDLE_ACTION_NAME_STYLING);
+      styleClasses.setAll(
+          ACTION_NAME_STYLING,
+          active ? START_ACTIVE_ACTION_NAME_STYLING : START_ACTION_NAME_STYLING);
+    } else if (index == children.size() - 1) {
+      styleClasses.setAll(
+          ACTION_NAME_STYLING, active ? END_ACTIVE_ACTION_NAME_STYLING : END_ACTION_NAME_STYLING);
+    } else if (index > 0 && index < children.size() - 1) {
+      styleClasses.setAll(
+          ACTION_NAME_STYLING,
+          active ? MIDDLE_ACTIVE_ACTION_NAME_STYLING : MIDDLE_ACTION_NAME_STYLING);
     } else {
-      // Optional: Logge den Fehler, um zu verstehen, wann dies auftritt
       System.err.println(
           "Ungültiger Index in setPositionalStyling: "
               + index
