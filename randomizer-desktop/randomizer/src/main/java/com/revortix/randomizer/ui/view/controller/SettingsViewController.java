80c6e646c784f614fdc44fcdd5903780e05f01e4
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-03-05 13:02:12 +0100

    feat: add functionality for view switching inside settings

--- randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/SettingsViewController.java
+++ randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/SettingsViewController.java
@@ -1,6 +1,52 @@
 package com.revortix.randomizer.ui.view.controller;
 
+import com.google.inject.Inject;
 import com.revortix.randomizer.ui.view.View;
+import com.revortix.randomizer.ui.view.ViewProvider;
+import com.revortix.randomizer.ui.view.controller.settings.GeneralSettingsController;
+import com.revortix.randomizer.ui.view.controller.settings.UpdaterSettingsViewController;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.GridPane;
 
 @View
-public class SettingsViewController {}
+public class SettingsViewController {
+
+  private final ViewProvider viewProvider;
+
+  @FXML private ToggleButton generalToggleButton;
+  @FXML private ToggleButton updaterToggleButton;
+  @FXML private GridPane contentPane;
+
+  @Inject
+  public SettingsViewController(ViewProvider viewProvider) {
+    this.viewProvider = viewProvider;
+  }
+
+  @FXML
+  private void initialize() {
+    generalToggleButton
+        .selectedProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              updaterToggleButton.setSelected(!newVal);
+              if (newVal) {
+                contentPane
+                    .getChildren()
+                    .setAll(viewProvider.requestView(GeneralSettingsController.class).parent());
+              }
+            });
+
+    updaterToggleButton
+        .selectedProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              generalToggleButton.setSelected(!newVal);
+              if (newVal) {
+                contentPane
+                    .getChildren()
+                    .setAll(viewProvider.requestView(UpdaterSettingsViewController.class).parent());
+              }
+            });
+  }
+}
---
+++ randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/settings/GeneralSettingsController.java
@@ -0,0 +1,6 @@
+package com.revortix.randomizer.ui.view.controller.settings;
+
+import com.revortix.randomizer.ui.view.View;
+
+@View
+public class GeneralSettingsController {}
