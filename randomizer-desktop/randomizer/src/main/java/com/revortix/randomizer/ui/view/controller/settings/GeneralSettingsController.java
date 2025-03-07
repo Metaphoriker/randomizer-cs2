08b07af690ab4e340c70ff5670a05c9fb39420be
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-03-08 00:24:34 +0100

    feat: add first logic of general settings

--- randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/settings/GeneralSettingsController.java
+++ randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/settings/GeneralSettingsController.java
@@ -1,6 +1,29 @@
 package com.revortix.randomizer.ui.view.controller.settings;
 
+import com.google.inject.Inject;
+import com.revortix.randomizer.config.RandomizerConfig;
 import com.revortix.randomizer.ui.view.View;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
 
 @View
-public class GeneralSettingsController {}
+public class GeneralSettingsController {
+
+  private final RandomizerConfig randomizerConfig;
+
+  @FXML private ToggleButton showIntroToggleButton;
+
+  @Inject
+  public GeneralSettingsController(RandomizerConfig randomizerConfig) {
+    this.randomizerConfig = randomizerConfig;
+  }
+
+  @FXML
+  private void initialize() {
+    showIntroToggleButton.setSelected(randomizerConfig.isShowIntro());
+
+    showIntroToggleButton
+        .selectedProperty()
+        .addListener((obs, oldVal, newVal) -> randomizerConfig.setShowIntro(newVal));
+  }
+}
