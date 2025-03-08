1dddbc4b234a3b9f4b9c00118793559f9741c464
Benjamin Sommerfeld <luziferisdoof@gmail.com>
2025-03-08 01:49:57 +0100

    feat: add sync button

--- randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/settings/GeneralSettingsController.java
+++ randomizer-desktop/randomizer/src/main/java/com/revortix/randomizer/ui/view/controller/settings/GeneralSettingsController.java
@@ -5,6 +5,7 @@ import com.revortix.randomizer.ui.view.View;
 import com.revortix.randomizer.ui.view.viewmodel.settings.GeneralSettingsViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.Tooltip;
 
 @View
 public class GeneralSettingsController {
@@ -12,6 +13,7 @@ public class GeneralSettingsController {
   private final GeneralSettingsViewModel generalSettingsViewModel;
 
   @FXML private ToggleButton showIntroToggleButton;
+  @FXML private ToggleButton syncConfigButton;
 
   @Inject
   public GeneralSettingsController(GeneralSettingsViewModel generalSettingsViewModel) {
@@ -20,6 +22,7 @@ public class GeneralSettingsController {
 
   @FXML
   private void initialize() {
+    syncConfigButton.setTooltip(new Tooltip("Sync"));
     showIntroToggleButton
         .selectedProperty()
         .bindBidirectional(generalSettingsViewModel.showIntroProperty());
